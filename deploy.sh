90f98165f14e2bbd4629fb01381e001e30472637
P.Kernevez <pkernevez@octo.com>
2015-12-29 15:52:17 +0100

    Correction pour flume

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ AWS_IP=`echo $DOCKER_HOST | sed -E "s/.*\/([0-9\.]+):.*/\1/"`
 
 
 echo "Cleaning existing container"
-docker rm -f kafka cff_sniff
+docker rm -f kafka cff_sniff flume
 
 
 echo "Now deploy new infrastructure"
 
 ### KAFKA
 docker build -t octoch/kafka components/kafka
-docker run -d -p 2181:2181 -p 9092:9092 --name kafka -h kafka --net=cff_realtime --env KAFKA_HEAP_OPTS="-Xmx256M -Xms128M" --env ADVERTISED_HOST=kafka --env ADVERTISED_PORT=9092 --volumes-from kafka_data jenkins_dataoctoch/kafka
+docker run -d -p 2181:2181 -p 9092:9092 --name kafka -h kafka --net=cff_realtime --env KAFKA_HEAP_OPTS="-Xmx256M -Xms128M" --env ADVERTISED_HOST=kafka --env ADVERTISED_PORT=9092 --volumes-from kafka_data octoch/kafka
 #docker exec -it kafka bash
 
 # Test Kafka :
@@ -22,14 +22,14 @@ docker run -d -p 2181:2181 -p 9092:9092 --name kafka -h kafka --net=cff_realtime
 # docker run --rm=true -it --net=cff_realtime -w /opt/kafka_2.11-0.8.2.1/bin/ spotify/kafka bash
 # ./kafka-console-producer.sh --broker-list kafka:9092 --topic test
 
+#flume
+# cd components/flume-dump
+docker build -t octoch/flume components/flume-dump
+docker run -e FLUME_AGENT_NAME=agent_kafka_dump -e FLUME_CONF_FILE=/var/tmp/flume.conf -h flume --net=cff_realtime  -d --name flume octoch/flume
+
 # SNIFFER
 docker build -t octoch/cff_sniff components/node_cff_realtime_sniffer
 docker run --env KAFKA_HOST=kafka --env MODE=$MODE --net=cff_realtime -h cff_sniff -d --name cff_sniff octoch/cff_sniff
-
-#flume
-# cd components/flume-dump
-docker build -t flume .
-docker run -e FLUME_AGENT_NAME=agent_kafka_dump -e FLUME_CONF_FILE=/var/tmp/flume.conf -h flume --net=cff_realtime  -d --name flume flume
 
 echo "Deployed successfully"
 echo "Connect on : http://$AWS_IP"
