ea5635006f649f7493672a73ce0fe1e8cf9b0f7e
Alexandre Masselot <alexandre.masselot@gmail.com>
2016-04-05 06:24:27 +0200

    renaming/cleaning

--- components/play-real-time/src/scala/com/octo/suisse/cffrealtime/models/Train.scala
+++
@@ -1,6 +0,0 @@
-package com.octo.suisse.cffrealtime.models
-
-/**
- * Created by alex on 30/12/15.
- */
-case class Train(id: String, name: String, category: String, lastStopName: String)
--- components/play-real-time/src/scala/com/octo/suisse/cffrealtime/models/TrainPosition.scala
+++
@@ -1,6 +0,0 @@
-package com.octo.suisse.cffrealtime.models
-
-/**
- * Created by alex on 30/12/15.
- */
-case class TrainPosition(train: Train, position: GeoCoordinates)
